471f8e5ae05ffe11a7320922f3702d8ced22d79d
iTomKo <tomkotaken@pm.me>
2026-01-15 22:13:43 +0100

    Rewritten AndroidLogger to use NDK

--- rust/librespot-ffi/src/logger.rs
+++ rust/librespot-ffi/src/logger.rs
@@ -1,28 +1,48 @@
 use jni::{JNIEnv, JavaVM};
+use log::{Level, LevelFilter, Metadata, Record};
+use std::ffi::CString;
 
-use log::{LevelFilter, Metadata, Record};
+const ANDROID_LOG_VERBOSE: i32 = 2;
+const ANDROID_LOG_DEBUG: i32 = 3;
+const ANDROID_LOG_INFO: i32 = 4;
+const ANDROID_LOG_WARN: i32 = 5;
+const ANDROID_LOG_ERROR: i32 = 6;
+
+unsafe extern "C" {
+    fn __android_log_write(prio: i32, tag: *const i8, text: *const i8) -> i32;
+}
 
 pub struct AndroidLogger {
+    #[allow(dead_code)]
     jvm: JavaVM,
 }
 
 impl AndroidLogger {
+    /// Initialize the global logger.
     pub fn init(jvm: JavaVM, level: LevelFilter) -> Result<(), log::SetLoggerError> {
         let logger = AndroidLogger { jvm };
         log::set_boxed_logger(Box::new(logger))?;
         log::set_max_level(level);
         Ok(())
     }
+}
 
-    fn log_to_android(env: &mut JNIEnv, level: log::Level, target: &str, msg: &str) {
-        let tag = target;
-        match level {
-            log::Level::Error => crate::logger::log_error(env, tag, msg),
-            log::Level::Warn => crate::logger::log_warn(env, tag, msg),
-            log::Level::Info => crate::logger::log_info(env, tag, msg),
-            log::Level::Debug => crate::logger::log_debug(env, tag, msg),
-            log::Level::Trace => crate::logger::log_verbose(env, tag, msg),
-        }
+fn log_to_ndk(level: Level, tag: &str, msg: &str) {
+    let tag = if tag.is_empty() { "rust" } else { tag };
+    let tag_c = CString::new(tag).unwrap_or_else(|_| CString::new("rust").unwrap());
+    // if message contains NULs, fall back to a safe placeholder
+    let msg_c = CString::new(msg).unwrap_or_else(|_| CString::new("invalid log message").unwrap());
+
+    let prio = match level {
+        Level::Error => ANDROID_LOG_ERROR,
+        Level::Warn => ANDROID_LOG_WARN,
+        Level::Info => ANDROID_LOG_INFO,
+        Level::Debug => ANDROID_LOG_DEBUG,
+        Level::Trace => ANDROID_LOG_VERBOSE,
+    };
+
+    unsafe {
+        let _ = __android_log_write(prio, tag_c.as_ptr() as *const i8, msg_c.as_ptr() as *const i8);
     }
 }
 
@@ -36,69 +56,47 @@ impl log::Log for AndroidLogger {
             return;
         }
 
-        if let Ok(mut env) = self.jvm.attach_current_thread_as_daemon() {
-            Self::log_to_android(
-                &mut env,
-                record.level(),
-                record.target(),
-                &format!("{}", record.args()),
-            );
-        }
+        let tag = record.target();
+        let msg = format!("{}", record.args());
+        log_to_ndk(record.level(), tag, &msg);
     }
+
     fn flush(&self) {}
 }
 
-/// Internal helper to call Log::<method>
-fn log_internal(env: &mut JNIEnv, method: &str, tag: &str, msg: &str) {
-    let jtag = env
-        .new_string(tag)
-        .expect("Couldn't create tag java string");
-    let jmsg = env
-        .new_string(msg)
-        .expect("Couldn't create msg java string");
-
-    let log_class = env
-        .find_class("android/util/Log")
-        .expect("Couldn't find android.util.Log");
-
-    env.call_static_method(
-        log_class,
-        method,
-        "(Ljava/lang/String;Ljava/lang/String;)I",
-        &[(&jtag).into(), (&jmsg).into()],
-    )
-    .expect("android.util.Log call failed");
+/// Legacy-compatible functions that accept a JNIEnv (but *do not* use it).
+/// This preserves your existing call sites/macros that pass `env`.
+pub fn log_verbose(tag: &str, msg: &str) {
+    log_to_ndk(Level::Trace, tag, msg);
 }
 
-pub fn log_verbose(env: &mut JNIEnv, tag: &str, msg: &str) {
-    log_internal(env, "v", tag, msg);
+pub fn log_debug(tag: &str, msg: &str) {
+    log_to_ndk(Level::Debug, tag, msg);
 }
 
-pub fn log_debug(env: &mut JNIEnv, tag: &str, msg: &str) {
-    log_internal(env, "d", tag, msg);
+pub fn log_info(tag: &str, msg: &str) {
+    log_to_ndk(Level::Info, tag, msg);
 }
 
-pub fn log_info(env: &mut JNIEnv, tag: &str, msg: &str) {
-    log_internal(env, "i", tag, msg);
+pub fn log_warn(tag: &str, msg: &str) {
+    log_to_ndk(Level::Warn, tag, msg);
 }
 
-pub fn log_warn(env: &mut JNIEnv, tag: &str, msg: &str) {
-    log_internal(env, "w", tag, msg);
+pub fn log_error(tag: &str, msg: &str) {
+    log_to_ndk(Level::Error, tag, msg);
 }
 
-pub fn log_error(env: &mut JNIEnv, tag: &str, msg: &str) {
-    log_internal(env, "e", tag, msg);
-}
-
+/// Macros preserved from your original code so call-sites don't need to change.
+/// They still accept an env argument but it's ignored by the implementation above.
 #[macro_export]
 macro_rules! logv {
     ($env:expr, $tag:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_verbose(&mut $env, $tag, &msg);
+        $crate::logger::log_verbose($tag, &msg);
     }};
     ($env:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_verbose(&mut $env, module_path!(), &msg);
+        $crate::logger::log_verbose(module_path!(), &msg);
     }};
 }
 
@@ -106,11 +104,11 @@ macro_rules! logv {
 macro_rules! logd {
     ($env:expr, $tag:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_debug(&mut $env, $tag, &msg);
+        $crate::logger::log_debug($tag, &msg);
     }};
     ($env:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_debug(&mut $env, module_path!(), &msg);
+        $crate::logger::log_debug(module_path!(), &msg);
     }};
 }
 
@@ -118,11 +116,11 @@ macro_rules! logd {
 macro_rules! logi {
     ($env:expr, $tag:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_info(&mut $env, $tag, &msg);
+        $crate::logger::log_info($tag, &msg);
     }};
     ($env:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_info(&mut $env, module_path!(), &msg);
+        $crate::logger::log_info(module_path!(), &msg);
     }};
 }
 
@@ -130,11 +128,11 @@ macro_rules! logi {
 macro_rules! logw {
     ($env:expr, $tag:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_warn(&mut $env, $tag, &msg);
+        $crate::logger::log_warn($tag, &msg);
     }};
     ($env:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_warn(&mut $env, module_path!(), &msg);
+        $crate::logger::log_warn(module_path!(), &msg);
     }};
 }
 
@@ -142,10 +140,11 @@ macro_rules! logw {
 macro_rules! loge {
     ($env:expr, $tag:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_error(&mut $env, $tag, &msg);
+        $crate::logger::log_error($tag, &msg);
     }};
     ($env:expr, $fmt:expr $(, $args:expr)* $(,)?) => {{
         let msg = format!($fmt $(, $args)*);
-        $crate::logger::log_error(&mut $env, module_path!(), &msg);
+        $crate::logger::log_error(module_path!(), &msg);
     }};
 }
+
