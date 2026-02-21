ad09e4a5cba2b0c407a99f8d2dc1e11d7492c81e
iTomKo <tomkotaken@pm.me>
2026-02-21 19:33:55 +0100

    ffi: added user metadata

---
+++ rust/librespot-ffi/src/metadata/user.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct UserJson {
+    pub uri: String,
+    #[serde(default)]
+    pub name: String,
+    #[serde(default)]
+    pub image_url: String,
+    pub followers_count: i32,
+    pub following_count: i32,
+    #[serde(default)]
+    pub is_following: bool,
+    pub color: i32,
+    #[serde(default)]
+    pub allow_follows: bool,
+    #[serde(default)]
+    pub show_follows: bool,
+
+    #[serde(default)]
+    pub total_public_playlists_count: i32,
+    #[serde(default)]
+    pub public_playlists: Vec<UserPublicPlaylistJson>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct UserPublicPlaylistJson {
+    pub uri: String,
+    pub name: String,
+    #[serde(default)]
+    pub image_url: String,
+    #[serde(default)]
+    pub followers_count: i32,
+    pub owner_uri: String,
+    #[serde(default)]
+    pub is_following: bool,
+}
